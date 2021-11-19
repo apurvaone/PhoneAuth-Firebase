e543479b5b0a13a6a5f845f7303441cb07037712
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-11-19 19:04:42 +0530

    PhoneAuthActivity.kt added

---
+++ app/src/main/java/com/example/phoneauth/PhoneAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.example.phoneauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_auth)
+    }
+}
